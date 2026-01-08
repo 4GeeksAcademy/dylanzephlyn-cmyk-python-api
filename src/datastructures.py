c9c0b65d17278a0799e0b9d784f24d135b417ca0
Dylan Steven Inchaurregui Dooley <dylan.zephlyn@hotmail.com>
2026-01-08 13:22:11 +0000

    actualizado

--- src/datastructures.py
+++ src/datastructures.py
@@ -15,21 +15,21 @@ class FamilyStructure:
                 "last_name": self.last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22],
-                "id": self._generateId()
+                "id": self._generate_Id()
             },
             {
                 "first_name": "Jane",
                 "last_name": self.last_name,
                 "age": 35,
                 "lucky_numbers": [10, 14, 3],
-                "id": self._generateId()
+                "id": self._generate_Id()
             },
             {
                 "first_name": "Jimmy",
                 "last_name": self.last_name,
                 "age": 5,
                 "lucky_numbers": [1],
-                "id": self._generateId()
+                "id": self._generate_Id()
             }
         ]
 
