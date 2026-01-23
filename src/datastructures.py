5f15f44df454030d400d9a4fb9db812904268cd7
Dylan Steven Inchaurregui Dooley <dylan.zephlyn@hotmail.com>
2026-01-23 19:30:55 +0000

    echo

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,9 +1,4 @@
-"""
-Update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
+
 
 class FamilyStructure:
     def __init__(self, last_name):
@@ -33,7 +28,7 @@ class FamilyStructure:
             }
         ]
 
-    # This method generates a unique incremental ID
+
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
@@ -58,6 +53,5 @@ class FamilyStructure:
                     return member
         return
 
-    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
